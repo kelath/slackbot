8b6fc31dbceb4d6e27f0440e51c183a21efff57b
Jay <kelath@gmail>
2016-06-01 16:12:52 -0700

    Start of slackbot

--- slackbot/plugins/secrefs.py
+++ slackbot/plugins/secrefs.py
@@ -1,1 +1,9 @@
-__author__ = 'bob'
+__author__ = 'jayk'
+
+import re
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+
+@respond_to('secbot', re.IGNORECASE)
+def hello_reply(message):
+    message.reply('hello human!')
