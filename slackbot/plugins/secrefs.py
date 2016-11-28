131d612e3d60cb77ed8ac29fd720fec76a04f489
Jay Kelath <jay.kelath@dowjones.com>
2016-11-27 20:29:51 -0500

    add more commands

--- slackbot/plugins/secrefs.py
+++ slackbot/plugins/secrefs.py
@@ -4,10 +4,12 @@ import re
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 
+
 @respond_to('secbot', re.IGNORECASE)
 def hello_reply(message):
     message.reply('hello human!')
 
-@respond_to('teach me (.*)')
-def teachme(message, something):
+
+@respond_to('what is (.*)')
+def whatis(message, something):
     message.reply('Here is {}'.format(something))
